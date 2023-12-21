938b60171497cff01d419a198c61c600568278d6
hesse65 <152978535+hesse65@users.noreply.github.com>
2023-12-21 12:03:55 +0100

    Update App.java

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,13 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+    System.out.println("low"); 
+
+    Rechner rechner= new Rechner();
+    System.out.println(rechner.add(2, 2));
+
+
     }
 
   
